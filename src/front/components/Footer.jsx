d6c4d8731fbfa261726ab0df42e4a43989fb6b12
DreenLoon <diegoarmando.l.f@outlook.com>
2026-04-14 04:21:24 +0000

    Componente Footer de React creado y Css creado

--- src/front/components/Footer.jsx
+++ src/front/components/Footer.jsx
@@ -1,11 +1,77 @@
-export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Check the <a target="_blank" href="https://4geeks.com/docs/start/react-flask-template">template documentation</a> <i className="fa-solid fa-file"></i> for help.
-		</p>
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
-);
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const Footer = () => {
+  return (
+    <footer className="bg-fc-dark text-fc-light py-5 mt-5 border-top border-fc-red">
+      <div className="container">
+        <div className="row gy-4">
+          
+          {/* COLUMNA 1: LOGO Y DESCRIPCIÓN */}
+          <div className="col-12 col-md-4">
+            <Link to="/" className="text-decoration-none fs-3 fw-bold mb-3 d-inline-block">
+              <span className="text-fc-red">Flavor</span>
+              <span className="text-fc-light">Critic</span>
+            </Link>
+            <p className="text-secondary" style={{ maxWidth: "300px" }}>
+              La plataforma definitiva para amantes de la gastronomía. Comparte, descubre y critica los mejores sabores del mundo.
+            </p>
+          </div>
+
+          {/* COLUMNA 2: OVERVIEW (ENLACES) */}
+          <div className="col-6 col-md-4">
+            <h5 className="footer-title">Overview</h5>
+            <ul className="list-unstyled d-flex flex-column gap-2">
+              <li>
+                <Link to="/" className="footer-link">Home</Link>
+              </li>
+              <li>
+                <Link to="/categorias" className="footer-link">Categorías</Link>
+              </li>
+              <li>
+                <Link to="/about" className="footer-link">Sobre Nosotros</Link>
+              </li>
+              <li>
+                <Link to="/contacto" className="footer-link">Contacto</Link>
+              </li>
+            </ul>
+          </div>
+
+          {/* COLUMNA 3: FOLLOW US (REDES SOCIALES) */}
+          <div className="col-6 col-md-4">
+            <h5 className="footer-title">Follow Us</h5>
+            <div className="d-flex gap-4">
+              {/* INSTAGRAM */}
+              <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="social-icon" aria-label="Instagram">
+                <svg width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
+                  <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.282.11-.705.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"/>
+                </svg>
+              </a>
+
+              {/* FACEBOOK */}
+              <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="social-icon" aria-label="Facebook">
+                <svg width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
+                  <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+                </svg>
+              </a>
+
+              {/* X (TWITTER) */}
+              <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="social-icon" aria-label="X">
+                <svg width="24" height="24" fill="currentColor" viewBox="0 0 16 16">
+                  <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865l8.875 11.633Z"/>
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+
+        {/* COPYRIGHT */}
+        <div className="row mt-5 pt-4 border-top border-secondary text-center text-secondary">
+          <div className="col">
+            <small>&copy; {new Date().getFullYear()} FlavorCritic. Todos los derechos reservados.</small>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
